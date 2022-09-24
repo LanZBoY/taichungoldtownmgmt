bc725b6c2b8c6f67b1be0f4b91eec257f74fc908
WenTee <superjimalex@gmail.com>
2022-09-24 13:43:20 +0800

    slide photo

--- src/Components/TopBar.js
+++ src/Components/TopBar.js
@@ -1,17 +1,22 @@
 import React from "react";
-import { Nav } from "react-bootstrap/esm";
+import { Nav, Navbar } from "react-bootstrap/esm";
 
 const topbar = ({currentKey}) => {
-    console.log(currentKey);
     return(
-        <Nav variant="tabs" activeKey={currentKey}>
-            <Nav.Item>
-                <Nav.Link href="/" eventKey="homepage">首頁</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link href="/tasklist" eventKey="tasklist">任務列表</Nav.Link>
-            </Nav.Item>
-        </Nav>
+        <>
+            <Navbar sticky="top" bg="dark" variant="dark" expand='lg'>
+                <Navbar.Brand >臺中城中城後台管理系統</Navbar.Brand>
+            </Navbar>
+            <Nav variant="tabs" activeKey={currentKey}>
+                <Nav.Item>
+                    <Nav.Link href="/" eventKey="homepage">首頁</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link href="/tasklist" eventKey="tasklist">任務列表</Nav.Link>
+                </Nav.Item>
+            </Nav>
+        </>
+        
     )
 };
 
--- src/Page/HomePage.js
+++ src/Page/HomePage.js
@@ -1,12 +1,55 @@
 import React  from "react";
 import TopBar from '../Components/TopBar'
+import { Carousel } from "react-bootstrap";
+import photo1 from '../statics/ZjKCJl2.png'
+import photo2 from '../statics/1592851764_100097.jpg'
+import photo3 from '../statics/08b202e9ea8181249c0b525f281a773b.jfif'
 import 'bootstrap/dist/css/bootstrap.min.css';
 const HomePage = () => {
+    console.log(photo1)
     return(
-        <div>
+        <>
            <TopBar currentKey={'homepage'}/>
-        </div>
-        
+           <Carousel interval={5000}>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src={photo1}
+                    alt="First slide"
+                    />
+                    <Carousel.Caption>
+                    <h3>秋紅谷</h3>
+                    <p>位在市中心</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src={photo2}
+                    alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                    <h3>我不知道這是哪裡</h3>
+                    <p>但是很漂亮</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src={photo3}
+                    alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                    <h3>台中湖心亭</h3>
+                    <p>
+                        欣賞漂亮湖景
+                    </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        </>
     )
 };
 
