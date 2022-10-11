a3c67fb580f917c082e55253b9b3febcdb7a8481
WenTee <superjimalex@gmail.com>
2022-10-11 20:18:01 +0800

    新增功能實作

--- src/Page/Components/CardList.js
+++ src/Page/Components/CardList.js
@@ -1,17 +1,16 @@
 import React from "react";
-import { Card, Container, Button } from "react-bootstrap";
-const cardElement = () =>{
+import { Card, Button } from "react-bootstrap";
+const cardElement = ({data}) =>{
     return(
-        <Container>
-            <Card className="text-center">
-                <Card.Body>
-                    <Card.Title>第一張卡片</Card.Title>
-                    <Card.Text>這是第一張卡片什麼都沒有</Card.Text>
-                    <Button variant="warning">修改</Button>
-                    <Button variant="danger">刪除</Button>
-                </Card.Body>
-            </Card>
-        </Container>
+        <Card className="text-center">
+            <Card.Header>{data.taskTitle}</Card.Header>
+            <Card.Body>
+                <Card.Text>{data.taskDesc}</Card.Text>
+                <Card.Img/>
+                <Button variant="warning">修改</Button>
+                <Button variant="danger">刪除</Button>
+            </Card.Body>
+        </Card>
     )
 };
 
--- src/Page/TaskListPage.js
+++ src/Page/TaskListPage.js
@@ -1,17 +1,57 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 import TopBar from './Components/TopBar'
-// import firestore from "./utils/firebase";
+import firestore from "./utils/firebase";
+import { collection, getDocs } from "firebase/firestore";
 import CardList from './Components/CardList'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card, Container, Form, Modal } from "react-bootstrap";
 
 
-const tasklist = () =>{
+const TaskList = () =>{
+    const [tasks, setTasks] = useState([])
+    const [showItem, setShowItem] = useState(false)
+    
+    const handleShowItem = () => setShowItem(true)
+    const handleCloseItme = () => setShowItem(false)
+
+    // useEffect(()=>{
+    //     getDocs(collection(firestore, 'tasks')).then((result) =>{
+    //         result.docs.forEach((doc) =>{
+    //             setTasks((prev) =>{
+    //                 prev = [...prev, {id : doc.id, data: doc.data()}]
+    //                 return prev
+    //             })
+    //         })
+    //     });
+    // },[])
     return(
-        <div>
+        <>
             <TopBar currentKey={'tasklist'}/>
-            <CardList/>
-        </div>
+            <Container>
+                {tasks.map((doc)=> {
+                    return <CardList data={doc.data} key={doc.id}/>
+                })}
+                <Card onClick={handleShowItem} bg='success' className="text-center" text='white'>
+                    <Card.Header>新增項目</Card.Header>
+                </Card>
+            </Container>
+            {/* 新增控件 */}
+            <Modal show={showItem} onHide={handleCloseItme} size = 'lg'>
+                <Modal.Header closeButton>建立項目</Modal.Header>
+                <Modal.Body>
+                    <Form className="text-center">
+                        <Form.Group>
+                            <Form.Label>任務標題</Form.Label>
+                            <Form.Control type="text" size="lg"></Form.Control>
+                            <Form.Label>任務描述</Form.Label>
+                            <Form.Control as='textarea' rows={3}></Form.Control>
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+            </Modal>
+        </>
     )
 };
 
-export default tasklist;
+export default TaskList;
